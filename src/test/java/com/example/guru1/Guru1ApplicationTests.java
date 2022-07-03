f12ca0a10cdaf6732fda305c949525181b874f68
obenotmane <omar.benotmane@gmail.com>
2022-07-03 11:20:05 +0100

    Initial commit

---
+++ src/test/java/com/example/guru1/Guru1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guru1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Guru1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
